ac907e42ffb232aa55ce1e4df6860eab2896e249
Therandus93 <kaceysherrard@hotmail.com>
2016-12-16 19:12:07 +0000

    mineor change

--- Web_CW3/src/main/java/cw3/config/WebConfig.java
+++ Web_CW3/src/main/java/cw3/config/WebConfig.java
@@ -3,12 +3,13 @@ package cw3.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-
+@EnableWebMvc
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 	
@@ -16,6 +17,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+    
+    @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+        		.addResourceLocations("/resources/");
+	}
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
--- Web_CW3/src/main/java/cw3/controller/PageController.java
+++ Web_CW3/src/main/java/cw3/controller/PageController.java
@@ -7,18 +7,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 	
-	  @RequestMapping(value = {"/index"}, method = RequestMethod.GET)
-	    public ModelAndView homePage(){
-	        return new ModelAndView("index");
-	    }  
+	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }  
+	
+	@RequestMapping(value = {"/index"}, method = RequestMethod.GET)
+	public ModelAndView homePage(){
+	    return new ModelAndView("index");
+	}  
 	  
-	  @RequestMapping(value = {"/errorPage"}, method = RequestMethod.GET)
-	    public ModelAndView error(){
-	        return new ModelAndView("error");
-	   }  
+	@RequestMapping(value = {"/errorPage"}, method = RequestMethod.GET)
+	public ModelAndView error(){
+	    return new ModelAndView("error");
+	}  
 }
